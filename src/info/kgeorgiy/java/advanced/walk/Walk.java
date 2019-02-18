a6cfbdd8a8be86ee7748d2352caf4fac42d1fd30
Kirill <8338050@gmail.com>
2019-02-18 22:28:07 +0300

    fixup

---
+++ src/info/kgeorgiy/java/advanced/walk/Walk.java
@@ -0,0 +1,84 @@
+package info.kgeorgiy.java.advanced.walk;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Walk {
+    public static void main(String[] args) {
+        String inputFile = args[0];
+        String outputFile = args[1];
+        Set<Path> paths;
+        Set<String> checksum;
+
+        Walk walk = new Walk(inputFile, outputFile);
+
+        paths = walk.readFile();
+        checksum = walk.calculateChecksums(paths);
+
+
+        walk.writeFile(checksum);
+    }
+
+
+    private Walk(String iF, String oF) {
+        inputFile = iF;
+        outputFile = oF;
+    }
+
+    private String inputFile;
+
+    private String outputFile;
+
+    private Set<String> calculateChecksums(Set<Path> paths) {
+        int CHUNK_SIZE = 1024;
+        int FNV_32_INIT = 0x811c9dc5;
+        int FNV_32_PRIME = 0x01000193;
+        LinkedHashSet<String> checksums = new LinkedHashSet<>();
+        for (Path path : paths) {
+            try (FileInputStream inputStream = new FileInputStream(path.toFile())) {
+                byte[] chunk = new byte[CHUNK_SIZE];
+                int chunkLength;
+                int hv = FNV_32_INIT;
+                while ((chunkLength = inputStream.read(chunk)) != -1) {
+                    for (int i = 0; i < chunkLength; i++) {
+                        hv = (hv * FNV_32_PRIME) ^ (chunk[i] & 0xff);
+                    }
+                }
+                checksums.add(String.format("%08x %s\n", hv, path));
+            } catch (IOException e) {
+                checksums.add(String.format("%08x %s\n", 0, path));
+            }
+        }
+        return checksums;
+    }
+
+    private Set<Path> readFile() {
+        Set<Path> parsedPaths = new LinkedHashSet<>();
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(this.inputFile), StandardCharsets.UTF_8)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                parsedPaths.add(Paths.get(line));
+            }
+        } catch (IOException e) {
+            System.err.format("IOException: %s", e);
+        }
+        return parsedPaths;
+    }
+
+    private void writeFile(Set<String> answer) {
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(this.outputFile), StandardCharsets.UTF_8)) {
+            for (String path : answer) {
+                writer.write(path);
+            }
+        } catch (IOException e) {
+            System.err.format("IOException: %s", e);
+        }
+    }
+
+
+}
